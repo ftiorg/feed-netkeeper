1558cafb2595a776243aa36d1c7e7f64c868320e
kamino <kamino@imea.me>
2020-09-19 21:34:40 +0800

    实现http请求登录

--- luci-app-netkeeper-interception/luasrc/controller/netkeeper-interception.lua
+++ luci-app-netkeeper-interception/luasrc/controller/netkeeper-interception.lua
@@ -9,6 +9,7 @@ function index()
 	page.dependent = true
 	entry({"admin","services","netkeeper-interception","status"},call("act_status")).leaf=true
 	entry({"admin","services","netkeeper-interception","authreq"},call("act_authreq")).leaf=true
+	entry({"admin","services","netkeeper-interception","apstatus"},call("act_apstatus")).leaf=true
 	if string.gsub(luci.sys.exec("uci get netkeeper-interception.config.enabled_r"),"\n","") == "1" then
 		entry({"remote" },call("act_remote")).leaf=true
 	end
@@ -46,9 +47,40 @@ end
 
 function act_remote()
 	local e={}
-	e.test = 123
-	e.token = string.gsub(luci.sys.exec("uci get netkeeper-interception.config.token"),"\n","")
-	e.post = luci.http.formvalue("post")
+	if luci.http.formvalue("post") == string.gsub(luci.sys.exec("uci get netkeeper-interception.config.token"),"\n","") then
+		local account = luci.http.formvalue("account")
+		if account then
+			local r = io.open("/var/Last_AuthReq", "w")
+			r:write(account)
+			r:close()
+			e.result = true
+			e.message = "success"
+		else
+			e.result = false
+			e.message = "no account"
+		end
+	else
+		e.result = false
+		e.message = "token error"
+	end
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 end
+
+function act_apstatus()
+	local e={}
+	if string.gsub(luci.sys.exec("uci get wireless.client.disabled"), "\n", "") == "0" then
+		e.enabled = true
+		local ip = string.gsub(luci.sys.exec("ifconfig wlan1 | grep 'inet addr' | awk '{ print $2}' | awk -F: '{print $2}'"),"\n","")
+		if ip == "" then
+			e.message = "正在连接到无线网络..."
+		else
+			e.message = "已连接到: ".. string.gsub(luci.sys.exec("uci get wireless.client.ssid"), "\n", "") .." IP: " .. ip
+		end
+	else
+		e.enabled = false
+	end
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(e)
+end
+
--- luci-app-netkeeper-interception/luasrc/model/cbi/netkeeper-interception.lua
+++ luci-app-netkeeper-interception/luasrc/model/cbi/netkeeper-interception.lua
@@ -1,7 +1,7 @@
 local sys = require "luci.sys"
 
 m = Map("netkeeper-interception")
-m.title	= translate("Netkeeper Interception")
+m.title	= "校园网登录"
 m.description = translate("Netkeeper Account Interception Plugin")
 
 m:section(SimpleSection).template = "netkeeper-interception/netkeeper-interception_authreq"
@@ -23,12 +23,11 @@ end
 iface.optional = false
 iface.rmempty = false
 
-
 status = s:option(TextValue, "status", "运行状态", "本地账号拦截服务")
 status.template = "netkeeper-interception/netkeeper-interception_status"
 
 r = m:section(TypedSection, "netkeeper-interception")
-r.title = "远程账号拦截"
+r.title = "远程账号登录"
 r.addremove = false
 r.anonymous = true
 
@@ -39,5 +38,50 @@ enable_r.rmempty = false
 token = r:option(Value, "token", translate("HTTP token"), translate("HTTP request token"))
 token.optional = false
 token.rmempty = false
+
+c = m:section(TypedSection, "netkeeper-interception")
+c.title = "无线网络连接"
+c.addremove = false
+c.anonymous = true
+
+ssid = c:option(Value, "ssid","SSID", "无线网络名称")
+ssid.optional = false
+ssid.rmempty = false
+
+password = c:option(Value, "password", "密码", "无线网络密码")
+password.optional = false
+password.rmempty = false
+password.password = true
+
+apswitch = c:option(Button, "apswitch", "连接", "修改ssid和密码后请先保存")
+apswitch.inputtitle = string.gsub(sys.exec("uci get wireless.client.disabled"), "\n", "") == "0" and "断开连接" or "开始连接"
+function apswitch.write(self, section, value)
+	if string.gsub(sys.exec("uci get network.wwan"), "\n", "") ~= "interface" then
+		sys.call("uci set network.wwan=interface")
+		sys.call("uci set network.wwan.proto='dhcp'")
+		sys.call("uci commit network")
+		sys.call("/etc/init.d/network reload")
+	end
+	if string.gsub(sys.exec("uci get wireless.client.disabled"), "\n", "") == "0" then
+		sys.call("uci del wireless.client")
+		sys.call("uci commit wireless")
+		sys.call("wifi reload")
+	else
+		sys.call("uci set wireless.client=wifi-iface")
+		sys.call("uci set wireless.client.disabled='0'")
+		sys.call("uci set wireless.client.ssid='"..string.gsub(sys.exec("uci get netkeeper-interception.config.ssid"), "\n", "").."'")
+		sys.call("uci set wireless.client.encryption='sae'")
+		sys.call("uci set wireless.client.device='radio1'")
+		sys.call("uci set wireless.client.mode='sta'")
+		sys.call("uci set wireless.client.key='"..string.gsub(sys.exec("uci get netkeeper-interception.config.password"), "\n", "").."'")
+		sys.call("uci set wireless.client.network='wwan'")
+		sys.call("uci set wireless.client.encryption='psk2'")
+		sys.call("uci commit wireless")
+		sys.call("wifi reload")
+	end
+end
+
+apstatus = c:option(TextValue, "apstatus", "连接状态", "无线网络连接状态")
+apstatus.template = "netkeeper-interception/netkeeper-interception_apstatus"
 
 return m
